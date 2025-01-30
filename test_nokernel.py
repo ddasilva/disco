2e0063472a1111f31a2e02908c43f119e4b1ee67
Daniel da Silva <mail@danieldasilva.org>
2025-01-30 02:08:38 +0000

    Starting refactoring

--- test_nokernel.py
+++ test_nokernel.py
@@ -1,5 +1,10 @@
+import time
 import cupy as cp
-from libgputrace import trace_trajectory
+import pandas as pd
+
+import libgputrace 
+
+from scipy.constants import m_e
 
 
 EARTH_DIPOLE_B0 = -30e3   # nT
@@ -8,11 +13,20 @@ EARTH_DIPOLE_B0 = -30e3   # nT
     
 def main():
     """Main method of the program."""
+    config = libgputrace.TraceConfig(
+        max_iters=5000,          # number of iterations
+        dt=.1,                   # s
+        vpar_start=.1,           # earth radii / sec
+        mu=1e-36,                # first invariant, 1e9 (R_earth**2) A
+        particle_mass=m_e,
+    )
+    grid_spacing = 0.1 
+    
     # Setup axes and grid
     x_axis = cp.arange(-10, 10, grid_spacing)
     y_axis = cp.arange(-10, 10, grid_spacing)
     z_axis = cp.arange(-5, 5, grid_spacing)
-    axes = Axes(x_axis, y_axis, z_axis)
+    axes = libgputrace.Axes(x_axis, y_axis, z_axis)
     
     x_grid, y_grid, z_grid = cp.meshgrid(
         axes.x, axes.y, axes.z,
@@ -38,22 +52,22 @@ def main():
     pos_y = cp.zeros(pos_x.shape)
     pos_z = cp.zeros(pos_x.shape)
 
-    pos = Positions(pos_x, pos_y, pos_z)
+    pos = libgputrace.Positions(pos_x, pos_y, pos_z)
     print('Number of particles:', pos_x.size)
 
-    vpar = vpar_start * cp.ones(pos.x.shape)    
+    vpar = config.vpar_start * cp.ones(pos.x.shape)    
 
     # History of positions
-    hist = PositionHistory(
-        x=cp.zeros((MAX_ITERS, pos.x.size)),
-        y=cp.zeros((MAX_ITERS, pos.y.size)),
-        z=cp.zeros((MAX_ITERS, pos.y.size))
+    hist = libgputrace.PositionHistory(
+        x=cp.zeros((config.max_iters, pos.x.size)),
+        y=cp.zeros((config.max_iters, pos.y.size)),
+        z=cp.zeros((config.max_iters, pos.y.size))
     )
 
-    print('Iterations:', MAX_ITERS)
+    print('Iterations:', config.max_iters)
     
     start_time = time.time()
-    sim(pos, vpar, hist, Bx, By, Bz, Btot, Ex, Ey, Ez, axes)
+    libgputrace.trace_trajectory(config, pos, vpar, hist, Bx, By, Bz, Btot, Ex, Ey, Ez, axes)
     end_time = time.time()
     
     print('time = ', end_time - start_time, 's')
